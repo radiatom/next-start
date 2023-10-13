5257ff34b1ad8eca3a7a2bdb95fbd8895c8b8f0a
radiatom <arssenmozol@gmail.com>
2023-10-13 15:43:19 +0300

    added module.scss

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
-import Script from "next/script";
 import React from "react";
+import Layout from "../../components/layout";
 
 const FirstPost = () => {
     return (
@@ -9,15 +9,11 @@ const FirstPost = () => {
             <Head>
                 <title>First Post</title>
             </Head>
-            <Script
-                src="https://connect.facebook.net/en_US/sdk.js"
-                strategy="lazyOnload"
-                onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)}
-            />
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to home</Link>
             </h2>
+            <Layout/>
         </>
     );
 };
