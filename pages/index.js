adcfd37dbdb8c2d06eaa590e31325322f5f0b573
radiatom <arssenmozol@gmail.com>
2023-10-14 14:49:39 +0300

    get posts

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,18 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.scss";
 import { useState } from "react";
 import clsx from "clsx";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData,
+        },
+    };
+}
+
+export default function Home({ allPostsData }) {
     const [change, setChange] = useState(false);
 
     return (
@@ -18,7 +28,7 @@ export default function Home() {
                     //     [utilStyles.backGroundColorRed]: change === true,
                     //     [utilStyles.backGroundColorYellow]: change === false,
                     // })}
-                    className={change?utilStyles.backGroundColorRed:utilStyles.backGroundColorYellow}
+                    className={change ? utilStyles.backGroundColorRed : utilStyles.backGroundColorYellow}
                 >
                     Energetic developer with enthusiasm for creating project logic. I have the experience of a private
                     entrepreneur. Interested in developing and improving own programming skills. Ready to work in the office in
@@ -28,7 +38,22 @@ export default function Home() {
                     (This is a sample website - you’ll be building a site like this on{" "}
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
                 </p>
-                <button onClick={()=>setChange(!change)}>Changing color</button>
+                <button onClick={() => setChange(!change)}>Changing color</button>
+            </section>
+            
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            {title}
+                            <br />
+                            {id}
+                            <br />
+                            {date}
+                        </li>
+                    ))}
+                </ul>
             </section>
         </Layout>
     );
